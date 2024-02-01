0402365355abf75581e3cee501237965d03e2865
Fareeth John <fareethjohn@gmail.com>
2024-02-01 16:47:04 +0530

    Update CYFMonitorExt.java
    Sync method supported added for Android's getSensorData API

--- Android/CYFMonitorExt.java
+++ Android/CYFMonitorExt.java
@@ -8,8 +8,8 @@ interface SensorDataHandler
 }
 
 public class CYFMonitorExt {
-    long kSD_Sampling_Time = 1000;
-    int kSD_TimeOut = 30;
+    long kSD_Sampling_Time = 500;
+    int kSD_TimeOut = 60;
     String kSD_default_mobile = "default-mobile";
 
     synchronized void getSensorDataInHandler(SensorDataHandler h){
@@ -28,4 +28,21 @@ public class CYFMonitorExt {
         }
         h.handle(theSD);
     };
+
+    synchronized String getSensorData(){
+        String theSD = CYFMonitor.getSensorData();
+        if (theSD.equals(kSD_default_mobile)){
+            for (int i=0; i< kSD_TimeOut; i++){
+                try {
+                    Thread.sleep(kSD_Sampling_Time);
+                    theSD = CYFMonitor.getSensorData();
+                    if (!theSD.equals(kSD_default_mobile)){
+                        return theSD;
+                    }
+                }catch (InterruptedException ex){
+                }
+            }
+        }
+        return theSD;
+    }
 }
